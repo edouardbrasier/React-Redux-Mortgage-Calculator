60d77b01bf96822bb52137dac2952e17586c9e11
edouardbrasier <edouardbrasier@gmail.com>
2017-01-19 19:31:23 +0000

    'example'

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,26 @@ import React,{Component} from 'react'
 import Slider from 'material-ui/Slider'
 import NumericInput from 'react-numeric-input'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import NumberFormat from 'react-number-format';
 
 import injectTapEventPlugin from 'react-tap-event-plugin'
 // Needed for onTouchTap http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
-export default class App extends Component {
+function myFormat(num) {
+    //return '£'+ num ;
+    return num ;
+}
 
+export default class App extends Component {
   handleFirstSlider = (event, value) => {
     this.props.principalChangeAction(value);
   };
 
   handleInput = (event, value) => {
+    //.console.log('hahaha',typeof value)
+    value= 1 + value;
+    value = value.slice( 1 );
     this.props.principalChangeAction(value);
     this.setState({input: value});
   };
@@ -31,6 +39,8 @@ export default class App extends Component {
       max={this.props.Max_Principal}
       step={this.props.Step_Principal}
       value={this.props.Principal}
+      format={myFormat}
+      precision={2}
       onChange={this.handleInput}
       />
       <MuiThemeProvider>
@@ -42,7 +52,7 @@ export default class App extends Component {
       onChange={this.handleFirstSlider}
       />
       </MuiThemeProvider>
-      <div>The monthly cost is {this.props.Monthly_Cost_Initial}
+      <div>The monthly cost is <NumberFormat value={this.props.Monthly_Cost_Initial.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'£'} />
       </div>
       </div>
     );
